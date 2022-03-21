019d72b2afbd0b6ad0e194d2ed80bb98d8fd68ef
carol-profile <carolgitonga93@gmail.com>
2022-03-21 20:52:46 +0300

    edit checkout form

--- js/checkout.js
+++ js/checkout.js
@@ -2,3 +2,5 @@ var numberOfKgs = 500;
 console.log(numberOfKgs);
 numberOfKgs = numberOfKgs + 100;
 console.log(numberOfKgs);
+
+var paymentMethod = $('input:radio[name=paymentMethod]:checked').val();
