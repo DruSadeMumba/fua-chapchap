7a1172b2699cf143528ddfe89835198627dfa627
Dru Mumba <drumumba@Drus-MacBook-Pro.local>
2022-03-22 15:03:51 +0300

    Add customer comment functions

--- js/landing.js
+++ js/landing.js
@@ -1,5 +1,24 @@
 let today = new Date();
 let year = today.getFullYear();
 
-document.getElementById("license-year").innerHTML = year
+document.getElementById("license-year").innerHTML = year;
 
+function Reviews(name, comment) {
+    this.customerName = name;
+    this.customerComment = comment;
+}
+Reviews.prototype.fullReview = function(){
+    return this.customerName + " " + says + " " + this.customerComment;
+}
+$document.ready(function(){
+    $("form#cst-comm").submit(function(event){
+        event.preventDefault();
+
+        let inputtedCustomerName = $("input#customer-name").val();
+		let inputtedCustomerComment = $("textarea#comments").val();
+		
+		let newReview = new Reviews(inputtedCustomerName, inputtedCustomerComment);
+
+		$("ul#reviews").append("<li>" + fullReview() + "</li>");
+    })
+})
